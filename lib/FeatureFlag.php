f57309ab14a3fe1605149b4e6714fc443a2dc94e
eric <eeoneric@gmail.com>
2022-08-03 10:03:04 -0400

    uncomment

--- lib/FeatureFlag.php
+++ lib/FeatureFlag.php
@@ -86,94 +86,95 @@ class FeatureFlag
     
         return false;
     }
+
+    function _hash($key, $distinct_id, $salt = "")
+    {
+
+    }
+
+    function get_matching_variant($flag, $distinct_id)
+    {
+        $variants = variant_lookup_table($flag);
+
+        foreach ($variants as $variant) {
+            if (
+                _hash($flag["key"], $distinct_id, "variant") >= $variant["value_min"]
+                && _hash($flag["key"], $distinct_id, "variant") < $variant["value_max"]
+            ) {
+                return $variant["key"]
+            }
+        }
+
+        return null
+    }
+
+    function variant_lookup_table($feature_flag)
+    {
+        $lookup_table = [];
+        $value_min = 0;
+        $multivariates = (($feature_flag['filters'] ?? {})['multivariate'] ?? {})['variants'] ?? []
+        
+        foreach ($multivariates as $variant) {
+            $value_max = $value_min + $variant["rollout_percentage"] / 100;
+
+            array_push($lookup_table, [
+                "value_min" => $value_min,
+                "value_max" => $value_max,
+                "key" => $variant["key"]
+            ])
+            $value_min = $value_max;
+        }
+
+        return $lookup_table;
+    }
+
+    function match_feature_flag_properties($flag, $distinct_id, $properties)
+    {
+        $flag_conditions = ($flag["filters"] ?? {})["groups"] ?? [];
+        $is_inconclusive = false;
+
+        foreach ($flag_conditions as $condition) {
+            try {
+                if (is_condition_match($flag, $distinct_id, $condition, $properties)) {
+                    return get_matching_variant($flag, $distinct_id) ?? true;
+                }
+            } catch (InconclusiveMatchException $e) {
+                $is_inconclusive = true;
+            }
+        }
+
+        if $is_inconclusive {
+            throw new InconclusiveMatchException("Can't determine if feature flag is enabled or not with given properties");
+        }
+
+        return false;
+    }
+
+    function is_condition_match($feature_flag, $distinct_id, $condition, $properties)
+    {
+        $rollout_percentage = $condition["rollout_percentage"];
+
+        if count($condition['properties'] ?? []) > 0 {
+            foreach ($condition['properties'] as $property) {
+                if !match_property($property, $properties) {
+                    return false;
+                }
+            }
+
+            if !is_null($rollout_percentage) {
+                return true;
+            }
+        }
+
+        if !is_null($rollout_percentage) && _hash($feature_flag["key"], $distinct_id) > ($rollout_percentage / 100) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+
+
 }
-
-// function _hash($key, $distinct_id, $salt = "")
-// {
-
-// }
-
-// function get_matching_variant($flag, $distinct_id)
-// {
-//     $variants = variant_lookup_table($flag);
-
-//     foreach ($variants as $variant) {
-//         if (
-//             _hash($flag["key"], $distinct_id, "variant") >= $variant["value_min"]
-//             && _hash($flag["key"], $distinct_id, "variant") < $variant["value_max"]
-//         ) {
-//             return $variant["key"]
-//         }
-//     }
-
-//     return null
-// }
-
-// function variant_lookup_table($feature_flag)
-// {
-//     $lookup_table = [];
-//     $value_min = 0;
-//     $multivariates = (($feature_flag['filters'] ?? {})['multivariate'] ?? {})['variants'] ?? []
-    
-//     foreach ($multivariates as $variant) {
-//         $value_max = $value_min + $variant["rollout_percentage"] / 100;
-
-//         array_push($lookup_table, [
-//             "value_min" => $value_min,
-//             "value_max" => $value_max,
-//             "key" => $variant["key"]
-//         ])
-//         $value_min = $value_max;
-//     }
-
-//     return $lookup_table;
-// }
-
-// function match_feature_flag_properties($flag, $distinct_id, $properties)
-// {
-//     $flag_conditions = ($flag["filters"] ?? {})["groups"] ?? [];
-//     $is_inconclusive = false;
-
-//     foreach ($flag_conditions as $condition) {
-//         try {
-//             if (is_condition_match($flag, $distinct_id, $condition, $properties)) {
-//                 return get_matching_variant($flag, $distinct_id) ?? true;
-//             }
-//         } catch (InconclusiveMatchException $e) {
-//             $is_inconclusive = true;
-//         }
-//     }
-
-//     if $is_inconclusive {
-//         throw new InconclusiveMatchException("Can't determine if feature flag is enabled or not with given properties");
-//     }
-
-//     return false;
-// }
-
-// function is_condition_match($feature_flag, $distinct_id, $condition, $properties)
-// {
-//     $rollout_percentage = $condition["rollout_percentage"];
-
-//     if count($condition['properties'] ?? []) > 0 {
-//         foreach ($condition['properties'] as $property) {
-//             if !match_property($property, $properties) {
-//                 return false;
-//             }
-//         }
-
-//         if !is_null($rollout_percentage) {
-//             return true;
-//         }
-//     }
-
-//     if !is_null($rollout_percentage) && _hash($feature_flag["key"], $distinct_id) > ($rollout_percentage / 100) {
-//         return false;
-//     }
-
-//     return true;
-// }
-
-
-
-
